53fcfb070124de0ef1cc445f0f6f4864105202b1
g8row <aliogu23@gmail.com>
2024-11-21 21:19:17 +0200

    delete build folder and fixup setup bug

--- build/Desktop_Qt_6_8_0-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_cutefin_FILE /home/g8row/Documents/qt/cutefin/build/Desktop_Qt_6_8_0-Debug/cutefin)
-set(__QT_DEPLOY_TARGET_cutefin_TYPE EXECUTABLE)
--- homewindow.cpp
+++ homewindow.cpp
@@ -1,6 +1,5 @@
 #include "homewindow.h"
 #include "setupwindow.h"
-#include "mainconfig.h"
 
 #include <QSettings>
 
@@ -10,10 +9,9 @@ HomeWindow::HomeWindow(QWidget *parent)
     QSettings *settings = new QSettings("g8row", "cutefin");
     SetupWindow *setupWindow = nullptr;
     JellyfinApi* jellyfinApi = new JellyfinApi(this);
-    qDebug() << cutefin_VERSION_MAJOR << "." << cutefin_VERSION_MINOR;
     this->show();
     if(!settings->contains("server_name") or !settings->contains("access_token")){
-        setupWindow = new SetupWindow(settings, jellyfinApi, this);
+        setupWindow = new SetupWindow(settings, jellyfinApi, nullptr);
         connect(setupWindow, &SetupWindow::setupComplete, this, [this](){
             this->show();
         });
@@ -22,7 +20,7 @@ HomeWindow::HomeWindow(QWidget *parent)
         jellyfinApi->setUrl(settings->value("server_name").toString());
         jellyfinApi->setAccessToken(settings->value("access_token").toString());
     }
-
+    /*
     connect(jellyfinApi, &JellyfinApi::latestMoviesResponse, this, [this](QList<Movie> movies, bool success, const QString &errorMessage = QString()){
         if (success){
             for (auto& i:movies){
@@ -31,4 +29,5 @@ HomeWindow::HomeWindow(QWidget *parent)
         }
     });
     jellyfinApi->getLatestMovies();
+    */
 }
